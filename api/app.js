7e2e0280e7522922eb088dd832ce517774810bfd
Del1rius <timothy.aidan.barry@gmail.com>
2023-09-01 11:33:56 +0200

    Products and Server Setup

--- api/app.js
+++ api/app.js
@@ -1,10 +1,38 @@
 require("dotenv").config()
 const express = require("express")
-
+const { express, routes } = require("./controllers/index")
+const errorHandling = require("./middleware/errorHandling")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const cors =  require("cors")
 const app = express()
 const PORT = process.env.PORt || 4000
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Request-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Expose-Headers", "Authorization")
+})
+
+app.use(
+    express.static("./static"),
+    express.urlencoded({
+        extended: false,
+    }),
+    cookieParser(),
+    cors(),
+    routes,
+    errorHandling
+)
+
+routes.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./static/html/index.html"))
+})
+
 app.listen(PORT, ()=> {
-    console.log(`Server is Live on ${PORT}`)
+    console.log(`Server is Live on http://localhost:${PORT}`)
 })
 
--- api/models/products.js
+++ api/models/products.js
@@ -2,6 +2,67 @@ const db = require("../config/db.config")
 
 class Products {
     fetchProducts(req, res) {
-        const query = `SELECT prodID,`
+        const query = `SELECT prodID, prodName, quantity, price, category, prodUrl
+        FROM Products;`
+
+        db.query(query, (err, data) => {
+            if (err) throw err
+            res.status(200).json(data)
+        })
+    }
+
+    fetchProduct(req, res) {
+        const query = `SELECT prodID, prodName, quantity, price, category, prodUrl
+        FROM Products
+        WHERE prodID = '${req.params.id}';`
+
+        db.query(query, (err, data) => {
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                result,
+            })
+        })
+    }
+
+    addProduct(req, res) {
+        const query = `INSERT INTO Products SET ?;`
+
+        db.query(query, req.body, (err) => {
+            if (err) throw err
+
+            res.json({
+                status: res.statusCode,
+                message: "A Product has been added!"
+            })
+        })
+    }
+
+    updateProduct(req, res) {
+        const query = `UPDATE Products SET ? WHERE prodID = ?;`
+
+        db.query(query, [req.body, req.params.id], (err) => {
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                message: "A Product Has Been Updated!"
+            })
+        })
+    }
+
+    deleteProduct(req, res) {
+        const query = `DELETE FROM Products WHERE prodID = '${req.params.id}';`
+
+        db.query(query, (err) => {
+            if (err) throw err
+
+            res.json({
+                status: res.statusCode,
+                message: "A PRODUCT HAS BEEN REMOVED FROM THE RECORDS!"
+            })
+        })
     }
 }
+
+
+module.exports = Products;
