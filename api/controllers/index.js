4d7fb06741509081e818bf218150787fd6d7adfd
Del1rius <timothy.aidan.barry@gmail.com>
2023-09-04 12:41:42 +0200

    changed a few things

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,83 +1,84 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const route = express.Router()
-const { users, products, orders } = require("../models")
+import express from ('express');
+const bodyParser = require("body-parser");
+const routes = express.Router();
+// Import all models objects
+const {users, products, orders} = require("../model");
 const verifyAToken = require("../middleware/authenticateUser")
 
-route.get("/users", (req, res) => {
+routes.get("/users", (req, res) => {
     users.fetchUsers(req, res)
 })
 
-route.get("/user/:id", (req, res) => {
+routes.get("/user/:id", (req, res) => {
     users.fetchUser(req, res)
 })
 
-route.post("/register", bodyParser.json(), (req, res) => {
+routes.post("/register", bodyParser.json(), (req, res) => {
     users.register(req, res)
 })
 
-route.post("/login", bodyParser.json(), (req, res) => {
+routes.post("/login", bodyParser.json(), (req, res) => {
     users.login(req, res)
 })
 
-route.put("/user/:id", bodyParser.json(), (req, res) => {
+routes.put("/user/:id", bodyParser.json(), (req, res) => {
     users.updateUser(req, res)
 })
 
-route.patch("/user/:id", bodyParser.json(), (req, res) => {
+routes.patch("/user/:id", bodyParser.json(), (req, res) => {
     users.updateUser(req, res)
 })
 
-route.get("/products", (req, res) => {
+routes.get("/products", (req, res) => {
     products.fetchProducts(req, res)
 })
 
-route.get("/products/:id", (req, res) => {
+routes.get("/products/:id", (req, res) => {
     products.fetchProduct(req, res)
 })
 
-route.post("/product", (req, res) => {
+routes.post("/product", (req, res) => {
     products.addProduct(req, res)
 })
 
-route.put("/product/:id", (req, res) => {
+routes.put("/product/:id", (req, res) => {
     products.updateProduct(req, res)
 })
 
-route.patch("product/:id", (req, res) => {
+routes.patch("product/:id", (req, res) => {
     products.updateProduct(req, res)
 })
 
-route.delete("product/:id", (req, res) => {
+routes.delete("product/:id", (req, res) => {
     products.deleteProduct
 })
 
-route.get("/orders", (req, res) => [
+routes.get("/orders", (req, res) => [
     orders.fetchOrders(req, res)
 ])
 
-route.get("/user/:id/carts", (req, res) => {
+routes.get("/user/:id/carts", (req, res) => {
     orders.fetchCart(req, res)
 })
 
-route.post("/user/:id/cart", bodyParser.json(), (req, res) => {
+routes.post("/user/:id/cart", bodyParser.json(), (req, res) => {
     orders.addToCart(req, res)
 })
 
-route.put("/user/:id/cart/:id", bodyParser.json(), (req, res) => {
+routes.put("/user/:id/cart/:id", bodyParser.json(), (req, res) => {
     orders.updateCart(req, res)
 })
 
-route.patch("/user/:id/cart/:id", bodyParser.json(), (req, res) => {
+routes.patch("/user/:id/cart/:id", bodyParser.json(), (req, res) => {
     orders.updateCart(req, res)
 })
 
-route.delete("/user/:id/cart", (req, res) => {
+routes.delete("/user/:id/cart", (req, res) => {
     orders.clearCart(req, res)
 })
 
-route.delete("/user/:id/cart/:id", (req, res) => {
+routes.delete("/user/:id/cart/:id", (req, res) => {
     orders.removeFromCart(req, res)
 })
 
-module.exports = {express, route}
+module.exports = {express, routes}
