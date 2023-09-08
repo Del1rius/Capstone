32a0545d3b0d255602f8150b69ca4e552e08eba6
Del1rius <timothy.aidan.barry@gmail.com>
2023-09-08 16:10:46 +0200

    start of contact

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="container-fluid text-center bg-black main">
+    <div class="container-fluid bg-black main">
+        <h2 class="display-3 text-center">
+                Contact Us
+        </h2>
+        <div class="row">
+            <div class="col-6 ms-5 text-center mt-2 block-1">
 
+            </div>
+            <div class="col-5 ms-5 text-center mt-2 block-2">
+
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -9,7 +19,25 @@ export default {
 }
 </script>
 <style>
-    .main {
-        min-height: 100vh;
-    }
+.main {
+    min-height: 100vh;
+}
+
+h2 {
+    color: #EEBC1D;
+}
+
+.block-1 {
+    min-height: 75vh;
+    border: 4px solid #EEBC1D;
+    border-radius: 25px;
+}
+
+.block-2 {
+    min-height: 75vh;
+    border: 4px solid #EEBC1D;
+    border-radius: 25px;
+}
+
+
 </style>
