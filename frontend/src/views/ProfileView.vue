d2317c2e0cd36d691ae4bdf27fee41650a46f36b
Del1rius <timothy.aidan.barry@gmail.com>
2023-09-14 12:18:29 +0200

    finished login and register

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,13 +1,122 @@
-<template lang="">
-    <div>
+<template>
+    <div class="container-fluid text-center bg-black main">
+        <h2 class="display-3">
+            User Profile:
+        </h2>
         
+        <div class="card text-center mx-auto">
+            <div class="mb-3">
+                <img :src="$store.state.user?.userImg" :alt="$store.state.user?.firstName" class="img-fluid img mt-2">
+            </div>
+
+            <div class="row">
+                <div class="mb-3 col-3">
+                    <h2 class="display-6 mb-1">Name:</h2>
+                    <h3 class="fs-3">
+                    {{ $store.state.user?.firstName }} {{ $store.state.user?.lastName }}
+                    </h3>
+                </div>
+                <div class="mb-3 col-3">
+                    <h2 class="display-6 mb-1">
+                        User Role:
+                    </h2>
+                    <h3 class="fs-3">
+                    {{ $store.state.user?.userRole }}
+                    </h3>
+                </div>
+
+                <div class="mb-3 col-3">
+                    <h2 class="display-6 mb-1">
+                        Email:
+                    </h2>
+                    <h3 class="fs-3">
+                    {{ $store.state.user?.emailAdd }}
+                    </h3>
+                </div>
+                <div class="mb-3 col-3">
+                    <h2 class="display-6 mb-1">
+                        Password:
+                    </h2>
+                    <h3 class="fs-3">
+                    <!-- {{ $store.state.user?.userPass }} -->
+                    </h3>
+                </div>
+            </div>
+
+            <div class="text-center p-2 m-2">
+                <UpdateUserComp class="btn edit-btn"/>
+                <button type="submit" class="btn logout-btn fs-4"><router-link to="/logout">Logout</router-link></button>
+                <button type="submit" class="btn delete-btn fs-4" @click="deleteUser(user.userID)">Delete Account</button>
+            </div>
+
+        </div>
     </div>
 </template>
 <script>
+import UpdateUserComp from "../components/UpdateUserComp.vue"
+
 export default {
-    
+    components: {
+        UpdateUserComp
+    },
+
+    computed: {
+        user() {
+            return this.$store.state.user
+        },
+
+        mounted() {
+            return this.$store.dispatch("fetchUser")
+        },
+    },
+
+    methods: {
+        deleteUser(userID) {
+            if (confirm("Are You Sure You Want TO Delete Your Account?")) {
+                this.$store.dispatch("deleteUser", userID);
+                setTimeout(() => {
+                    location.reload();
+                }, 500)
+            }
+        }
+    }
 }
 </script>
-<style lang="">
-    
+<style scoped>
+.main {
+    min-height: 100vh;
+    color: #eebc1d !important;
+} 
+
+h2 {
+    color: #eebc1d !important;
+}
+
+h3 {
+    color: #eebc1d !important;
+}
+.card {
+    background-color: black;
+    border: 3px solid #eebc1d;
+}
+
+.delete-btn {
+    color: #eebc1d !important;
+    border: 2px solid #eebc1d;
+    padding: 5px;
+    border-radius: 10px;
+}
+
+a {
+    text-decoration: none;
+    color: #eebc1d !important;
+    border: 2px solid #eebc1d;
+    padding: 8px;
+    border-radius: 10px;
+}
+
+.img {
+    border: 3px solid #eebc1d;
+    border-radius: 10px;
+}
 </style>
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,13 +1,124 @@
-<template >
-    <div>
-        
+<template>
+  <div class="container-fluid text-center bg-black main">
+    <div class="card">
+      <h2 class="display-3 mb-3">Register</h2>
+
+      <form @submit.prevent="registerUser" class="p-3">
+        <div class="m-3">
+          <label for="firstName" class="form-label">First Name:</label>
+          <input
+            v-model="user.firstName"
+            type="text"
+            id="firstName"
+            class="form-control"
+            placeholder="Enter your First Name Here"
+            required
+            oninvalid="this.setCustomValidity('Please Enter Your Name')"
+            oninput="this.setCustomValidity('')"
+          />
+        </div>
+        <div class="m-3">
+          <label for="lastName" class="form-label">Surname:</label>
+          <input
+            v-model="user.lastName"
+            type="text"
+            id="lastName"
+            class="form-control"
+            placeholder="Enter your Surname Here"
+            required
+            oninvalid="this.setCustomValidity('Please Enter Your Surname')"
+            oninput="this.setCustomValidity('')"
+          />
+        </div>
+
+        <div class="m-3">
+          <label for="emailAdd" class="form-label">Email: </label>
+          <input
+            v-model="user.emailAdd"
+            type="text"
+            id="emailAdd"
+            class="form-control"
+            placeholder="@gmail.com"
+            required
+            oninvalid="this.setCustomValidity('Please Enter Your Email')"
+            oninput="this.setCustomValidity('')"
+          />
+        </div>
+        <div class="m-3">
+          <label for="userPass" class="form-label">Password: </label>
+          <input
+            v-model="user.userPass"
+            type="text"
+            id="password"
+            class="form-control"
+            required
+            oninvalid="this.setCustomValidity('Please Enter A Password')"
+            oninput="this.setCustomValidity('')"
+          />
+        </div>
+        <div class="m-3">
+          <label for="userImg" class="form-label">User Image: </label>
+          <input
+            v-model="user.userImg"
+            type="text"
+            id="userImg"
+            placeholder="Insert Image Link"
+            class="form-control"
+          />
+        </div>
+        <div class="text-center mt-2">
+            <button type="submit" class="p-2 btn register-btn m-3">
+                Register
+            </button>
+            <button type="reset" class="p-2 btn reset-btn m-3">Reset</button>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      user: {
+        firstName: "",
+        lastName: "",
+        emailAdd: "",
+        userPass: "",
+        userImg: "",
+        userRole: "",
+      },
+    };
+  },
+
+  methods: {
+    registerUser() {
+      this.$store.dispatch("registerUser", this.user);
+      this.$router.push("/");
+    },
+  },
+};
 </script>
-<style >
-    
+<style>
+.main {
+  min-height: 100vh;
+  color: #eebc1d !important;
+}
+
+.card {
+  background-color: black;
+  color: #eebc1d;
+}
+
+.reset-btn {
+    background-color: black;
+    color: #eebc1d;
+    border: 2px solid #eebc1d;
+}
+
+.register-btn {
+    background-color: black;
+    color: #eebc1d;
+    border: 2px solid #eebc1d;
+}
 </style>
